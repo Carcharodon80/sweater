8d478e78385b0837ad461e7be90a5c82f17c6568
Carcharodon80 <karna241280@gmail.com>
2023-04-11 15:25:17 +0300

    Видео 2. Spring Boot JPA (Hibernate) - добавили БД, реализовали добавление сообщений, настроили отображение сообщений по фильтру тэгов.

---
+++ src/main/java/com/carcharodon80/sweater/MessageController.java
@@ -0,0 +1,60 @@
+package com.carcharodon80.sweater;
+
+import com.carcharodon80.sweater.domain.Message;
+import com.carcharodon80.sweater.repos.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MessageController {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    /**
+     * Метод запускается, когда приходит Get-запрос на "messages" из браузера
+     * @param model - модель, куда передаются сведения для отображения страницы ("messages" понадобится на "messages.html")
+     * @return - имя страницы из resources/templates
+     */
+    @GetMapping("/messages")
+    public String showMessages(Model model) {
+        Iterable<Message> messages = messageRepository.findAll();
+        model.addAttribute("messages", messages);
+        return "messages";
+    }
+
+    /**
+     * Метод запускается, когда приходит post-запрос на "addMessage" (свойство action у кнопки в messages.html)
+     * @param text - см. кнопку в messages.html
+     * @param tag - см. кнопку в messages.html
+     * @param model - модель для передачи сведений на страницу
+     * @return - здесь redirect, чтобы при обновлении страницы не происходило повторное добавление сообщения в БД
+     */
+    @PostMapping("/addMessage")
+    public String addMessage(@RequestParam String text, @RequestParam String tag, Model model) {
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+        return "redirect:messages";
+    }
+
+    /**
+     * Метод запускается, когда приходит post-запрос на "filter" (свойство action у кнопки в messages.html)
+     * @param filter - см. кнопку в messages.html
+     * @param model - модель для передачи сведений на страницу
+     * @return - отображение messages.html
+     */
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Model model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages", messages);
+        return "messages";
+    }
+}
---
+++ src/main/java/com/carcharodon80/sweater/repos/MessageRepository.java
@@ -0,0 +1,13 @@
+package com.carcharodon80.sweater.repos;
+
+import com.carcharodon80.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
